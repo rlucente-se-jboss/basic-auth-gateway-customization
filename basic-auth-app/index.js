7e938a411fc084e9731e9db6136a27c0abd700b7
Brandon Cox <bcox@redhat.com>
2017-05-23 13:10:59 -0400

    first commit

---
+++ basic-auth-app/index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var auth = require('basic-auth')
+var app = express()
+
+app.get('/', function (req, res) {
+
+  var credentials = auth(req)
+ 
+  if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+  } else {
+    res.end('Access granted')
+  }
+
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
